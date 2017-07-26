9ec21a21ea112f248d558d9d72e2384147494021
Denis Starovoitenko <30273945+StrakDD@users.noreply.github.com>
2017-07-26 23:38:23 +0300

    Initial commit

---
+++ src/main/java/ua/home/work/Main.java
@@ -0,0 +1,8 @@
+package ua.home.work;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+    }
+}
---
+++ src/main/java/ua/home/work/model/Entity.java
@@ -0,0 +1,7 @@
+package ua.home.work.model;
+
+/**
+ * Created by Denis Starovoitenko on 26.07.2017.
+ */
+public class Entity {
+}
