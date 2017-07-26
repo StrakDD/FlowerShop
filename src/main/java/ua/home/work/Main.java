d5b10c48976fcea361ed798aef5fe1de77f4c539
Denis Starovoitenko <30273945+StrakDD@users.noreply.github.com>
2017-07-27 02:18:59 +0300

    Add Model.Entity!

--- src/main/java/ua/home/work/Main.java
+++ src/main/java/ua/home/work/Main.java
@@ -1,8 +1,29 @@
 package ua.home.work;
 
-public class Main
-{
-    public static void main( String[] args )
-    {
+import ua.home.work.model.entity.Bouquet;
+import ua.home.work.model.entity.Instances.InstancesEvent;
+import ua.home.work.model.entity.Instances.InstancesFresh;
+import ua.home.work.model.entity.Instances.InstancesMoney;
+import ua.home.work.model.entity.Instances.InstancesStalk;
+
+public class Main {
+
+    public static void main( String[] args ) {
+        Bouquet bouquet1 = InstancesMoney.VIP.getInstance();
+        Bouquet bouquet2 = InstancesMoney.EXPENSIVE.getInstance();
+        Bouquet bouquet3 = InstancesMoney.CHEAP.getInstance();
+
+        Bouquet bouquet4 = InstancesEvent.WEDDING.getInstance();
+        Bouquet bouquet5 = InstancesEvent.SIMPLE.getInstance();
+
+        Bouquet bouquet6 = InstancesStalk.LONG.getInstance();
+        Bouquet bouquet7 = InstancesStalk.ABSENCE.getInstance();
+
+        Bouquet bouquet8 = InstancesFresh.FRESH.getInstance();
+        Bouquet bouquet9 = InstancesFresh.FADE.getInstance();
+
+
+        System.out.println(bouquet1);
+        System.out.println(bouquet4);
     }
 }
--- src/main/java/ua/home/work/model/Entity.java
+++
@@ -1,7 +0,0 @@
-package ua.home.work.model;
-
-/**
- * Created by Denis Starovoitenko on 26.07.2017.
- */
-public class Entity {
-}
